389ff0aada2a049a8be1057cfb0cfbd7e8112e95
OsmanthusPeace <3139714892@qq.com>
2025-02-07 13:50:52 +0800

    feat(git): use automated tool

---
+++ release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  branches: ["main"], // 仅在 main 分支发布
+  plugins: [
+    "@semantic-release/commit-analyzer", // 分析 commit 记录，确定版本号（major（使用feat!）/minor/patch）
+    "@semantic-release/release-notes-generator", // 生成 release notes
+    "@semantic-release/changelog", // 更新 CHANGELOG.md
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: true, // 修改 package.json 版本，并发布 npm
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
